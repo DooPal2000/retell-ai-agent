459ef6e86539a5e643f66c9780678d8f211408e5
Kyoung Yeob <kkyub1313@gmail.com>
2025-04-29 18:20:52 +0900

    feat(rotes): route 2종 수정 및 추가

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,26 +1,26 @@
-import express from 'express';
-import passport from 'passport';
-import { Router } from 'express';
+// import express from 'express';
+// import passport from 'passport';
+// import { Router } from 'express';
 
-import catchAsync from '../utils/catchAsync';
-import { isLoggedIn, isSecureAdminCreation, isAuthorized } from '../middleware';
-import products from '../controllers/Cproduct';
+// import catchAsync from '../utils/catchAsync';
+// import { isLoggedIn, isSecureAdminCreation, isAuthorized } from '../middleware';
+// import products from '../controllers/Cproduct';
 
-const router: Router = express.Router();
+// const router: Router = express.Router();
 
-router.route('/')
-    .get(isAuthorized, catchAsync(products.index))
-    .post(isAuthorized, catchAsync(products.createProduct));
+// router.route('/')
+//     .get(isAuthorized, catchAsync(products.index))
+//     .post(isAuthorized, catchAsync(products.createProduct));
 
-router.get('/new', isAuthorized, products.renderNewForm);
+// router.get('/new', isAuthorized, products.renderNewForm);
 
-router.route('/:id')
-    .get(isAuthorized, catchAsync(products.showProduct))
-    .put(isAuthorized, catchAsync(products.updateProduct))
-    .delete(isAuthorized, catchAsync(products.deleteProduct));
+// router.route('/:id')
+//     .get(isAuthorized, catchAsync(products.showProduct))
+//     .put(isAuthorized, catchAsync(products.updateProduct))
+//     .delete(isAuthorized, catchAsync(products.deleteProduct));
 
-router.get('/:id/edit', isAuthorized, catchAsync(products.renderEditForm));
+// router.get('/:id/edit', isAuthorized, catchAsync(products.renderEditForm));
 
-router.post('/:id/update-quantity', isAuthorized, catchAsync(products.updateQuantity));
+// router.post('/:id/update-quantity', isAuthorized, catchAsync(products.updateQuantity));
 
-export default router; 
+// export default router; 
